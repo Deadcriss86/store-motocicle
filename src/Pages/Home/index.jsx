4ab89606213537d9bc82bedd6bd6f99cf1515a02
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-03-07 10:06:27 -0600

    add api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,24 @@
-import Layout from "../../components/Layout"
-import Card from "../../components/card"
+import { useState, useEffect } from "react";
+import Layout from "../../components/Layout";
+import Card from "../../components/card";
+import { data } from "autoprefixer";
 
 function Home() {
-    return (
-       <Layout>
-            <Card/>
-       </Layout>
-    )
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((Response) => Response.json())
+      .then((data) => setItems(data));
+  }, []);
+  return (
+    <Layout>
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
-export default Home
+export default Home;
