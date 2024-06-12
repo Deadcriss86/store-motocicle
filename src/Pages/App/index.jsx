5e559d804a3f8ebd9206978528cfb880e280a231
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-06-12 10:42:58 -0600

    login y register beta1

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter, useRoutes } from "react-router-dom";
 import Home from "../Home";
 import NotFound from "../NotFound";
+import Login from "../LoginUser";
+import Singup from "../Register";
 import "./App.css";
 
 const AppRoutes = () => {
   const routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "*", element: <NotFound /> },
+    { path: "/login", element: <Login /> },
+    { path: "/Singup", element: <Singup /> },
   ]);
   return routes;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customColors: {
+          light: "#FFECB3",
+          DEFAULT: "#FFC107",
+          darkGray: "#0d0d0d",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
