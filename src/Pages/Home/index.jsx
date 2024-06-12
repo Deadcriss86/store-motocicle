060081c2cf20e19f0a521b258eb7be2bcd96362c
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-06-11 21:30:07 -0600

    pages

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,15 @@
 function Home() {
   return (
     <>
-      <h1 className="bg-blue">home</h1>
+      <div className="border-yellow-400 h-1/2 w-1/2 bg-yellow-300 rounded-sm px-2">
+        <ul className="text-white flex justify-between">
+          <li>Productos</li>
+          <li>Atencion a cliente</li>
+          <li>Mis compras</li>
+          <li>🛒</li>
+        </ul>
+      </div>
+      <h1 className="">home</h1>
     </>
   );
 }
