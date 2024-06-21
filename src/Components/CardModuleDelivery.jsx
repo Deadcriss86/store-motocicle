b842851e32fb2bba62430a33575a29a0ab01a555
Dante Sotelo <136533308+dxnte1234@users.noreply.github.com>
2024-06-20 19:19:22 -0600

    create module for Cards Buys

---
+++ src/Components/CardModuleDelivery.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { CiEdit } from "react-icons/ci";
+import '/Users/dxnte/Desktop/Kodeawards/store-motocicle/src/Pages/Home/OrderPages/styleOrder.css'
+
+const CardDelivery = ({ deliveryDescription, nameClient, priceDelivery, descriptionGuide }) => {
+    return(
+        <div className="cardContainer">
+            <div className="noPedido">Pedido No. 13432</div>
+            <div className="clientName">Nombre : Alejandro Medina</div>
+            <div className="mountTotal">Monto total:$ 5,000.00</div>
+            <div className="guideNumber">No.guia:
+                <div className="numberGuide"></div>
+            </div>
+            <div className="saveButton">
+                <button>Guardar</button>
+            </div>
+            <button className="editButton"><CiEdit /></button>
+        </div>
+    )
+}
+export default CardDelivery;
