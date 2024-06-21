fc72c4386ecd839d14474390f1b890d9fe7acd5b
Dante Sotelo <136533308+dxnte1234@users.noreply.github.com>
2024-06-21 16:21:59 -0600

    fix imports and adding structure

--- src/Pages/Home/CostumerServicePage/CostumerSer.jsx
+++ src/Pages/Home/CostumerServicePage/CostumerSer.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '/Users/dxnte/Desktop/Kodeawards/store-motocicle/src/Pages/Home/CostumerServicePage/styleCust.css';
+import { CiUser } from "react-icons/ci";
+import { LiaCartArrowDownSolid } from "react-icons/lia";
+import logo from '../../../assets/logo2.png'
+import './styleCust.css';
 
 function CostumerSer() {
   return (
-    <div>
-      
+    <div className='containerFather'>
+      <div className='containerNavbar'>
+        <img className='h-20' src={logo} alt="logo" />
+        <nav>
+          <a href="#">Productos</a>
+          <a href="#">Atencion al cliente</a>
+          <a href="#">Mis compras</a>
+          <a href="#">Perfil<CiUser /></a>
+          <button><LiaCartArrowDownSolid /></button>
+        </nav>
+        <button>Logout</button>
+      </div>
     </div>
   )
 }
