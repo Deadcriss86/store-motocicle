bdd1ec2fd4604f0b593d516ed9d5994f579cee8e
Dante Sotelo <136533308+dxnte1234@users.noreply.github.com>
2024-06-20 19:18:56 -0600

    add structure for functions

--- src/Pages/Home/OrderPages/OrderPages.jsx
+++ src/Pages/Home/OrderPages/OrderPages.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from '../../../Components/SearchInput'
+import CardDelivery from '../../../Components/CardModuleDelivery'
 import '/Users/dxnte/Desktop/Kodeawards/store-motocicle/src/Pages/Home/OrderPages/styleOrder.css'
 
 function OrderPages() {
@@ -17,7 +18,13 @@ function OrderPages() {
           <SearchBar onSearch={handleSearch}/>
         </nav>
         <main className='mainContent'>
-          <p className='textMain'>Resultado de la busqueda: {searchQuery}</p>
+          <div className='containerText'>
+            <p className='textMain'>Resultado de la busqueda: {searchQuery}</p>
+          </div>
+          <CardDelivery/>
+          <CardDelivery/>
+          <CardDelivery/>
+          <CardDelivery/>
         </main>
         <footer className='footer'>
           <button className='buttonProduct'>Productos</button>
