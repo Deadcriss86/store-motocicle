f73c2870440cfcc955f818a563a3c70dfce303a1
curcomil <sadielsalgado98@gmail.com>
2024-06-22 14:38:02 -0600

    Productos y agregar productos. Instalar RHF

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -7,6 +7,7 @@ import Singup from "../Register";
 import ProductPage from "../Details";
 import "./App.css";
 import Shopping_cart from "../Shopping_cart";
+import Productos from "../Productos";
 
 const AppRoutes = () => {
   const routes = useRoutes([
@@ -16,7 +17,7 @@ const AppRoutes = () => {
     { path: "/login", element: <Login /> },
     { path: "/Singup", element: <Singup /> },
     { path: "/Detail", element: <ProductPage /> },
-
+    { path: "/Productos", element: <Productos /> },
   ]);
   return routes;
 };
