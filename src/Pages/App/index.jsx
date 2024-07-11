e394926610d47178c80d996c6bbcf179252f9798
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-07-11 16:12:26 -0600

    protect routes

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -3,24 +3,30 @@ import { BrowserRouter, useRoutes } from "react-router-dom";
 import Home from "../Home";
 import NotFound from "../NotFound";
 import Login from "../LoginUser";
-import Singup from "../Register";
+import Signup from "../Register"; // Corregido de Singup a Signup
 import ProductPage from "../Details";
 import "./App.css";
 import Shopping_cart from "../Shopping_cart";
 import Editor_user from "../UserEditor";
 import Productos from "../Productos";
 import { AuthProvider } from "../../context/AuthContext";
+import { ProtectedRoute } from "../../routes";
 
 const AppRoutes = () => {
   const routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "*", element: <NotFound /> },
-    { path: "/carrito", element: <Shopping_cart /> },
     { path: "/login", element: <Login /> },
-    { path: "/Signup", element: <Singup /> },
-    { path: "/Detail", element: <ProductPage /> },
-    { path: "/Editoruser", element: <Editor_user /> },
-    { path: "/Productos", element: <Productos /> },
+    { path: "/signup", element: <Signup /> }, // Corregido de /Singup a /signup
+    { path: "/detail", element: <ProductPage /> }, // Corregido de /Detail a /detail
+    { path: "/productos", element: <Productos /> }, // Corregido de /Productos a /productos
+    {
+      element: <ProtectedRoute />,
+      children: [
+        { path: "/editoruser", element: <Editor_user /> }, // Corregido de /Editoruser a /editoruser
+        { path: "/carrito/:id", element: <Shopping_cart /> },
+      ],
+    },
   ]);
   return routes;
 };
