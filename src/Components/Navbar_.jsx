344364ea0d8c34dd5b0332cb94d58ed1ea47c1a6
cristina hicks <hicks.arquitectura@gmail.com>
2024-07-21 14:22:14 -0600

    Cambios en la navbar funcionando

--- src/Components/Navbar_.jsx
+++ src/Components/Navbar_.jsx
@@ -36,6 +36,39 @@ export const Navlink = () => {
       >
         Mi perfil <FaRegUser className="mt-1" />
       </Link>
+      <button onClick={logout}>
+        <IoCartOutline
+          size={24}
+          className="text-gray-100 hover:text-[#0eff06] dark:text-white"
+        />
+      </button>
+      <button
+        onClick={logout}
+        className="nav-button hover:drop-shadow-lg flex w-auto items-center justify-center rounded-full border border-[#0eff06e9] bg-[#0eff06] bg-gradient-to-tr from-[#0eff06] to-[#78c048]/70 px-7 py-2.5 font-bold text-slate-800 ring-lime-600 ring-offset-2 ring-offset-slate-700 drop-shadow-[0px_1px_2px_rgb(0,0,0,0.3)] active:ring-1"
+      >
+        <span>Logout</span>
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 16 16"
+          className="ml-2"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        ></svg>
+      </button>
+    </div>
+  );
+
+  const GuestLinks = () => (
+    <div className="flex items-center space-x-8">
+      <Link
+        to="/Menu"
+        className="text-gray-100 hover:text-[#0eff06] dark:text-white"
+      >
+        Productos
+      </Link>
       <button>
         <IoCartOutline
           size={24}
@@ -43,35 +76,32 @@ export const Navlink = () => {
         />
       </button>
       <Link
-        to="/"
-        className="text-gray-900 bg-[#0eff06] hover:bg-[#0eff06b4] focus:ring-1 focus:outline-none focus:ring-[#0eff06] font-medium rounded-xl text-sm px-4 py-2 dark:bg-[#0eff06] dark:hover:bg-[#0eff069d] dark:focus:ring-[#0eff06]"
-        onClick={() => logout()}
+        to="/login"
+        className="nav-button hover:drop-shadow-lg flex w-auto items-center justify-center rounded-full border border-[#0eff06e9] bg-[#0eff06] bg-gradient-to-tr from-[#0eff06] to-[#78c048]/70 px-7 py-2.5 font-bold text-slate-800 ring-lime-600 ring-offset-2 ring-offset-slate-700 drop-shadow-[0px_1px_2px_rgb(0,0,0,0.3)] active:ring-1"
       >
-        Logout
+        <span>Login</span>
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 16 16"
+          className="ml-2"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+          ></path>
+        </svg>
       </Link>
     </div>
   );
 
-  const GuestLinks = () => (
-    <>
-      <Link
-        to="/Shopping"
-        className="text-gray-100 hover:text-[#0eff06] dark:text-white"
-      >
-        Mis
-      </Link>
-      <Link
-        to="/login"
-        className="text-gray-900 bg-[#0eff06] hover:bg-[#0eff06b4] focus:ring-1 focus:outline-none focus:ring-[#0eff06] font-medium rounded-xl text-sm px-4 py-2 mx-2 dark:bg-[#0eff06] dark:hover:bg-[#0eff069d] dark:focus:ring-[#0eff06]"
-      >
-        l
-      </Link>
-    </>
-  );
-
   const MobileMenu = () => (
     <div
-      className={`w-1/2 md:hidden rounded-xl  ${
+      className={`w-1/2 md:hidden rounded-xl ${
         isOpen ? "block" : "hidden"
       } absolute top-16 left-0 bg-[#272927e7] py-4`}
     >
@@ -84,66 +114,93 @@ export const Navlink = () => {
             Productos
           </Link>
         </li>
-        <li>
-          <Link
-            to="/serviceAtention"
-            className="text-gray-100 hover:text-[#0eff06] dark:text-white"
-          >
-            Atención al cliente
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/Shopping"
-            className="text-gray-100 hover:text-[#0eff06] dark:text-white"
-          >
-            Mis compras
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/editoruser"
-            className="text-gray-100 hover:text-[#0eff06] dark:text-white flex items-center"
-          >
-            Mi perfil <FaRegUser className="ml-1" />
-          </Link>
-        </li>
-        <li>
-          <Link>
-            <button>
-              <IoCartOutline
-                TO="/carrito"
-                size={24}
+        {isAuthenticated && (
+          <>
+            <li>
+              <Link
+                to="/serviceAtention"
                 className="text-gray-100 hover:text-[#0eff06] dark:text-white"
-              />
-            </button>
-          </Link>
-        </li>
+              >
+                Atención al cliente
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/Shopping"
+                className="text-gray-100 hover:text-[#0eff06] dark:text-white"
+              >
+                Mis compras
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/editoruser"
+                className="text-gray-100 hover:text-[#0eff06] dark:text-white flex items-center"
+              >
+                Mi perfil <FaRegUser className="ml-1" />
+              </Link>
+            </li>{" "}
+          </>
+        )}
         <li>
-          <Link
-            to="/login"
-            className="nav-button hover:drop-shadow-lg flex w-full items-center justify-center rounded-full border border-[#0eff06e9] bg-[#0eff06] bg-gradient-to-tr from-[#0eff06] to-[#78c048]/70 px-7 py-2.5 text-base font-bold text-slate-800 ring-lime-600 ring-offset-2 ring-offset-slate-700 drop-shadow-[0px_1px_2px_rgb(0,0,0,0.3)] active:ring-1"
-          >
-            <span>Login</span>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 16 16"
-              className="ml-2"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-              ></path>
-            </svg>
-          </Link>
+          <button>
+            <IoCartOutline
+              TO="/carrito"
+              size={24}
+              className="text-gray-100 hover:text-[#0eff06] dark:text-white"
+            />
+          </button>
         </li>
+        {isAuthenticated ? (
+          <li>
+            <button
+              onClick={logout}
+              className="nav-button hover:drop-shadow-lg flex w-full items-center justify-center rounded-full border border-[#0eff06e9] bg-[#0eff06] bg-gradient-to-tr from-[#0eff06] to-[#78c048]/70 px-7 py-2.5 text-base font-bold text-slate-800 ring-lime-600 ring-offset-2 ring-offset-slate-700 drop-shadow-[0px_1px_2px_rgb(0,0,0,0.3)] active:ring-1"
+            >
+              <span>Logout</span>
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 16 16"
+                className="ml-2"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                ></path>
+              </svg>
+            </button>
+          </li>
+        ) : (
+          <li>
+            <Link
+              to="/login"
+              className="nav-button hover:drop-shadow-lg flex w-full items-center justify-center rounded-full border border-[#0eff06e9] bg-[#0eff06] bg-gradient-to-tr from-[#0eff06] to-[#78c048]/70 px-7 py-2.5 text-base font-bold text-slate-800 ring-lime-600 ring-offset-2 ring-offset-slate-700 drop-shadow-[0px_1px_2px_rgb(0,0,0,0.3)] active:ring-1"
+            >
+              <span>Login</span>
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 16 16"
+                className="ml-2"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                ></path>
+              </svg>
+            </Link>
+          </li>
+        )}
       </ul>
-      {isAuthenticated ? <AuthenticatedLinks /> : <GuestLinks />}
     </div>
   );
 
@@ -161,62 +218,8 @@ export const Navlink = () => {
             {isOpen ? <IoClose size={24} /> : <IoMenu size={24} />}
           </button>
         </div>
-        <div className="hidden md:flex justify-center flex-1">
-          <div className="flex items-center space-x-8">
-            <Link
-              to="/Menu"
-              className="text-gray-100 hover:text-[#0eff06] md:hover:bg-transparent dark:text-white"
-            >
-              Productos
-            </Link>
-            <Link
-              to="/serviceAtention"
-              className="text-gray-100 hover:text-[#0eff06] md:hover:bg-transparent dark:text-white"
-            >
-              Atención al cliente
-            </Link>
-            <Link
-              to="/Shopping"
-              className="text-gray-100 hover:text-[#0eff06] md:hover:bg-transparent dark:text-white"
-            >
-              Mis compras
-            </Link>
-            <Link
-              to="/Editoruser"
-              className="text-gray-100 hover:text-[#0eff06] md:hover:bg-transparent dark:text-white flex gap-3 items-start"
-            >
-              Mi perfil <FaRegUser className="mt-1" />
-            </Link>
-            <button>
-              <IoCartOutline
-                size={24}
-                className="text-gray-100 hover:text-[#0eff06] md:hover:bg-transparent dark:text-white"
-              />
-            </button>
-          </div>
-        </div>
-        <div className="hidden md:flex">
-          <Link
-            to="/login"
-            className="nav-button hover:drop-shadow-lg flex w-full items-center justify-center rounded-full border border-[#0eff06e9] bg-[#0eff06] bg-gradient-to-tr from-[#0eff06] to-[#78c048]/70 px-7 py-2.5 text-base font-bold text-slate-800 ring-lime-600 ring-offset-2 ring-offset-slate-700 drop-shadow-[0px_1px_2px_rgb(0,0,0,0.3)] active:ring-1"
-          >
-            <span>Login</span>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 16 16"
-              className="ml-2"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-              ></path>
-            </svg>
-          </Link>
+        <div className="hidden md:flex justify-end flex-1 px-10">
+          {isAuthenticated ? <AuthenticatedLinks /> : <GuestLinks />}
         </div>
         <MobileMenu />
       </div>
