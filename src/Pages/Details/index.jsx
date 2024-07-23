5c3842f18199e3d865979adccafef263bb154e76
cristina hicks <hicks.arquitectura@gmail.com>
2024-07-22 23:13:45 -0600

    detalle producto modif front

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -47,7 +47,7 @@ const ProductPage = () => {
       <main className="p-4">
         <div className="max-w-5xl mx-auto">
           <div className="flex flex-col lg:flex-row lg:space-x-8 bg-gray-800 p-4 rounded-lg">
-            <div className="lg:w-1/2">
+            <div className="lg:w-1/2 bg-gray-400">
               <img
                 src={product?.images || "https://via.placeholder.com/400"}
                 alt="Producto"
@@ -59,8 +59,8 @@ const ProductPage = () => {
               <h1 className="text-3xl font-bold mb-2 text-[#0eff06]">
                 {product?.productName || "Nombre del producto"}
               </h1>
-              <p className="text-[#0eff06]">Precio</p>
-              <p className="text-2xl text-[#0eff06] mb-4">
+              <p className="text-white">Precio</p>
+              <p className="text-2xl text-[#0eff06] mb-4 border-b-2 border-[#0eff06]">
                 ${product?.price?.toFixed(2)} MXN
               </p>
               <p className="text-red-500 mb-4">
@@ -70,7 +70,7 @@ const ProductPage = () => {
                   : "Stock no disponible"}
                 )
               </p>
-              <button className="bg-[#0eff06] text-black font-bold py-2 px-4 rounded-full mb-4">
+              <button className="bg-[#0eff06] text-black font-bold py-2 px-4 rounded-full mb-4 hover:text-white hover:bg-gradient-to-r from-orange-300 to-[#0eff06] ">
                 Agregar al carrito
               </button>
               <div className="flex items-center mb-4">
@@ -82,30 +82,34 @@ const ProductPage = () => {
               <p className="mb-4">
                 ¡Envío de 3 a 5 días hábiles!* Hasta 12MSI con mercado crédito
               </p>
-              <button className="bg-gray-700 text-white font-bold py-2 px-4 rounded-full">
+              <button className="border-2 border-[#0eff06] text-[#0eff06] px-4 py-2 rounded-xl font-bold hover:text-gray-800 hover:bg-gradient-to-r from-orange-300 to-[#0eff06]">
                 Manual Instalación
               </button>
             </div>
           </div>
 
           <div className="bg-gray-800 p-4 mt-8 rounded-lg">
-            <div className="tabs mb-4">
-              <button className="px-4 py-2 bg-green-700 text-white rounded-l">
+            <details className="tabs mb-4">
+              <summary className="px-4 py-2 bg-[#0eff06] text-gray-900 font-bold rounded-xl">
                 Descripción
-              </button>
-              <button className="px-4 py-2 bg-gray-700 text-white">
-                Opiniones
-              </button>
-              <button className="px-4 py-2 bg-gray-700 text-white rounded-r">
-                Información del envío
-              </button>
-            </div>
-            <div className="tab-content text-gray-300">
-              <p>
+              </summary>
+              <ul className=" text-gray-300 text-justify m-4">
                 {product?.description ||
                   "Descripción no disponible. Slider tipo jaula. Hecho de acero industrial y con pintura electrostática para mayor duración, incluso en climas costeros..."}
-              </p>
-            </div>
+              </ul>
+            </details>
+            <details className="tabs mb-4">
+              <summary className="px-4 py-2 bg-[#0eff06] text-gray-900 font-bold rounded-xl">
+                Opiniones
+              </summary>
+              <ul className=" text-gray-300 text-justify m-4">Hola</ul>
+            </details>
+            <details className="tabs mb-4">
+              <summary className="px-4 py-2 bg-[#0eff06] text-gray-900 font-bold rounded-xl">
+                Información del envío
+              </summary>
+              <ul className=" text-gray-300 text-justify m-4">Hola otra vez</ul>
+            </details>
           </div>
 
           <div className="bg-gray-800 p-4 mt-8 rounded-lg">
@@ -116,7 +120,7 @@ const ProductPage = () => {
                 type="text"
                 placeholder="Hacer una pregunta..."
               />
-              <button className="bg-[#0eff06] text-white px-4 py-2 rounded-lg">
+              <button className="border-2 border-[#0eff06] text-[#0eff06] px-4 py-2 rounded-xl font-bold hover:text-gray-800 hover:bg-gradient-to-r from-orange-300 to-[#0eff06]">
                 Enviar Pregunta
               </button>
             </div>
