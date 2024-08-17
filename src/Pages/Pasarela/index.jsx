01531532ee8fafcd5071d56d675d6bccc6338ceb
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-08-17 15:12:37 -0600

    Update index.jsx

--- src/Pages/Pasarela/index.jsx
+++ src/Pages/Pasarela/index.jsx
@@ -1,7 +1,6 @@
 import CheckoutForm from "../../Components/Checkoutforms";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import "./styles.css";
 
 const stripePromise = loadStripe(
   "pk_test_51PoIHhRvRsZDGGXQtFoKdaPS4R5wx1JPv6LBB4sxo2VeNNgmGMVxHftnGvFbsCTQzhBxumNoAej9ysuid53PFomE00JEY4rQYf"
@@ -29,19 +28,23 @@ export const Pasarela = () => {
   const amount = calculateOrderAmount(items.items);
 
   return (
-    <div className="App">
-      <p>Por favor paga: ${amount / 100}</p>
-      <Elements
-        stripe={stripePromise}
-        options={{
-          mode: "payment",
-          amount: amount,
-          currency: "mxn",
-          appearance: { theme: "night" },
-        }}
-      >
-        <CheckoutForm items={items}></CheckoutForm>
-      </Elements>
+    <div className="flex items-center justify-center h-screen w-screen bg-gray-50">
+      <div className="w-[30vw] min-w-[500px] p-10 shadow-lg rounded-lg bg-white">
+        <p className="text-lg font-semibold text-center text-gray-700 mb-4">
+          Por favor paga: ${amount / 100}
+        </p>
+        <Elements
+          stripe={stripePromise}
+          options={{
+            mode: "payment",
+            amount: amount,
+            currency: "mxn",
+            appearance: { theme: "night" },
+          }}
+        >
+          <CheckoutForm items={items} />
+        </Elements>
+      </div>
     </div>
   );
 };
