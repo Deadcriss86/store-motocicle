0c00f9b0e2214dfad78cfee29d14280e105ed33b
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-08-17 11:54:04 -0600

    beta1

--- src/Pages/UserEditor/index.jsx
+++ src/Pages/UserEditor/index.jsx
@@ -20,6 +20,7 @@ const EditProfileForm = () => {
   const [showModal, setShowModal] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
   const [profileData, setProfileData] = useState(null);
+  const [selectedAvatar, setSelectedAvatar] = useState("avatar1.jpg");
 
   useEffect(() => {
     axios
@@ -27,7 +28,7 @@ const EditProfileForm = () => {
       .then((response) => {
         setProfileData(response.data);
         setValue("nombre", response.data.nombre);
-        setValue("apellidos", response.data.apellido);
+        setValue("apellido", response.data.apellido);
         setValue("nacionalidad", response.data.nacionalidad);
         setValue("movil", response.data.movil);
         setValue("cp", response.data.cp);
@@ -35,6 +36,7 @@ const EditProfileForm = () => {
         setValue("calle", response.data.calle);
         setValue("delegacion", response.data.delegacion);
         setValue("referencias", response.data.referencias);
+        setSelectedAvatar(response.data.avatar || "avatar1.jpg");
       })
       .catch((error) => {
         console.error("Error al obtener los datos del perfil", error);
@@ -43,9 +45,11 @@ const EditProfileForm = () => {
 
   const onSubmit = async (data) => {
     try {
-      await axios.put("http://localhost:3000/api/auth/update", data, {
-        withCredentials: true,
-      });
+      await axios.put(
+        "http://localhost:3000/api/auth/update",
+        { ...data, avatar: selectedAvatar },
+        { withCredentials: true }
+      );
       setModalMessage("Perfil actualizado con éxito!");
       setShowModal(true);
       reset();
@@ -53,6 +57,10 @@ const EditProfileForm = () => {
       setModalMessage("Error al actualizar el perfil: " + error.message);
       setShowModal(true);
     }
+  };
+
+  const handleAvatarSelect = (avatar) => {
+    setSelectedAvatar(avatar);
   };
 
   if (loading) {
@@ -74,14 +82,44 @@ const EditProfileForm = () => {
             </h2>
             <div className="flex justify-center mb-4">
               <div className="w-24 h-24 bg-gray-700 rounded-full flex items-center justify-center">
-                <svg
-                  className="w-16 h-16 text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 12c2.28 0 4-1.72 4-4s-1.72-4-4-4-4 1.72-4 4 1.72 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-                </svg>
+                <img
+                  src={`/assets/avatars/${selectedAvatar}`}
+                  alt="Avatar seleccionado"
+                  className="w-16 h-16 rounded-full"
+                />
               </div>
+            </div>
+            <div className="flex justify-center mb-4">
+              <img
+                src="../../../public/avatars/avatar1.jpg"
+                alt="Avatar 1"
+                className={`w-16 h-16 rounded-full mx-2 cursor-pointer ${
+                  selectedAvatar === "avatar1.jpg"
+                    ? "border-4 border-green-500"
+                    : ""
+                }`}
+                onClick={() => handleAvatarSelect("avatar1.jpg")}
+              />
+              <img
+                src="../../../public/avatars/avatar2.jpg"
+                alt="Avatar 2"
+                className={`w-16 h-16 rounded-full mx-2 cursor-pointer ${
+                  selectedAvatar === "avatar2.jpg"
+                    ? "border-4 border-green-500"
+                    : ""
+                }`}
+                onClick={() => handleAvatarSelect("avatar2.jpg")}
+              />
+              <img
+                src="../../../public/avatars/avatar3.jpg"
+                alt="Avatar 3"
+                className={`w-16 h-16 rounded-full mx-2 cursor-pointer ${
+                  selectedAvatar === "avatar3.jpg"
+                    ? "border-4 border-green-500"
+                    : ""
+                }`}
+                onClick={() => handleAvatarSelect("avatar3.jpg")}
+              />
             </div>
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mb-4 flex">
@@ -95,18 +133,18 @@ const EditProfileForm = () => {
                 />
                 <input
                   type="text"
-                  {...register("apellidos", {
+                  {...register("apellido", {
                     required: "Los apellidos son obligatorios",
                   })}
-                  placeholder={profileData?.apellidos || "Apellidos"}
+                  placeholder={profileData?.apellido || "Apellidos"}
                   className="bg-gray-800 text-white p-2 rounded-sm w-1/2 ml-1 focus:outline-none"
                 />
               </div>
               {errors.nombre && (
                 <p className="text-red-500">{errors.nombre.message}</p>
               )}
-              {errors.apellidos && (
-                <p className="text-red-500">{errors.apellidos.message}</p>
+              {errors.apellido && (
+                <p className="text-red-500">{errors.apellido.message}</p>
               )}
 
               <div className="mb-4 flex">
@@ -171,7 +209,7 @@ const EditProfileForm = () => {
                   {...register("ciudad", {
                     required: "La ciudad es obligatoria",
                   })}
-                  placeholder={profileData?.ciudad || "ciudad"}
+                  placeholder={profileData?.ciudad || "Ciudad"}
                   className="bg-gray-800 text-white p-2 rounded-sm w-full focus:outline-none mx-1"
                 />
               </div>
