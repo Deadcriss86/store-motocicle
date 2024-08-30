a03b293062a67891002eda9a781366d6d45b5972
Dante Sotelo <136533308+dxnte1234@users.noreply.github.com>
2024-08-29 20:54:38 -0600

    add page 401 Unauthorized

---
+++ src/Components/Unauthorized.jsx
@@ -0,0 +1,22 @@
+import imgfondo from "../../../dist/assets/img_fondo.png";
+import logo from "../../../dist/assets/logo2.png";
+
+const Unauthorized = () => {
+  return (
+    <div className="relative flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div
+        className="absolute bottom-0 w-full h-full bg-cover bg-center z-0"
+        style={{ backgroundImage: `url(${imgfondo})` }}
+      ></div>
+      <div className="absolute top-10 left-10 z-10">
+        <img src={logo} alt="Logo" className="h-16" />
+      </div>
+      <h1 className="text-9xl font-bold text-[#0eff06] z-10">401</h1>
+      <p className="text-xl text-[#0eff06] mt-4 z-10">
+        Lo siento no estas autorizado para ver esto.
+      </p>
+    </div>
+  );
+};
+
+export default Unauthorized;
