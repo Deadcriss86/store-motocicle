4e969ecba90984db31c65e5782fd77e5d540c17d
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-08-30 16:41:53 -0600

    fix register

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "./axios";
 
 export const registerRequest = async (user) =>
-  axios.post(`/auth/register`, user);
+  axios.post(`/api/auth/register`, user);
 
 export const loginRequest = async (user) => axios.post(`/api/auth/login`, user);
 
