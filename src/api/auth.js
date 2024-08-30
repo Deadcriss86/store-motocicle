41c356d3048f13e61659ac4ab2aa426b796db2ca
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-08-30 16:26:19 -0600

    delete get profile

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,5 +6,3 @@ export const registerRequest = async (user) =>
 export const loginRequest = async (user) => axios.post(`/api/auth/login`, user);
 
 export const verifyTokenRequest = async () => axios.get(`/api/auth/verify`);
-
-export const fetchProfile = async () => axios.get(`/api/auth/profile`);
