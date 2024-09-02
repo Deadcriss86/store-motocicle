ca07a5203f36fdcb021a506c059c9b73ded14655
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-09-02 14:13:19 -0600

    fix home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -24,19 +24,18 @@ function Home() {
   return (
     <>
       <Navlink />
-      <div className="bg-black min-h-screen">
-        <div className="relative w-full h-[calc(100vh-56px)]">
+      <div className="bg-black min-h-screen overflow-x-hidden">
+        <div className="relative w-full h-screen">
           <img
             className="w-full h-full object-cover opacity-90"
             alt="Imagen de fondo"
             src={imgfondo}
           />
-          <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-4 m-4 w-full">
+          <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-4 m-4">
             <TextRevealCard
               text="Venta de accesorios para tu motocicleta"
-              className="font-bold py-20 text-green bg-clip-text w-1/1 text-center"
+              className="font-bold py-20 text-green bg-clip-text"
             />
-
             <div className="text-base sm:text-lg flex items-center space-x-2 text-white">
               <span>Conoce nuestros productos</span>
               <Link to="/Menu">
@@ -55,43 +54,41 @@ function Home() {
               <button>Contáctanos para pedidos personalizados</button>
               <IoLogoWhatsapp
                 size="2.5rem"
-                className=" animate-bounce text-[#0eff06] hover:text-white"
+                className="animate-bounce text-[#0eff06] hover:text-white"
               />
             </a>
           </div>
         </div>
-        <div className="sticky top-0 h-auto py-5 flex flex-col items-center justify-center bg-black text-white">
+        <div className="bg-black text-white py-5 flex flex-col items-center justify-center">
           <div className="text-center text-white max-w-4xl space-y-4">
-            <br />
             <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 text-[#0eff06] border-[#0eff06] p-2">
               Quiénes somos
             </h2>
             <div className="border-b-2 border-[#0eff06] p-2 text-lg sm:text-xl md:text-2xl">
-              <p className="">
+              <p>
                 Somos una empresa dedicada a la fabricación de accesorios y
                 defensas para cualquier tipo de motocicleta. Nuestro principal
                 objetivo es que nuestros clientes se sientan satisfechos con la
                 estética personalizada de su moto.
               </p>
             </div>
-            <br />
             <SlBadge size="2rem" className="text-[#0eff06] inline-block" />
           </div>
-        </div>{" "}
-        <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-black">
-          <div className="text-[#0eff06] text-3xl font-bold flex justify-center py-4 text-center">
+        </div>
+        <div className="bg-black flex flex-col items-center justify-center h-screen">
+          <div className="text-[#0eff06] text-3xl font-bold py-4 text-center">
             Alguno de nuestros productos
           </div>
           <Carousel />
         </div>
-        <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-black">
-          <h2 className="text-white text-3xl font-bold flex justify-center py-8 text-center">
+        <div className="bg-black flex flex-col items-center justify-center h-screen">
+          <h2 className="text-white text-3xl font-bold py-8 text-center">
             Comentarios de nuestros clientes
           </h2>
           <Card_coment />
         </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
