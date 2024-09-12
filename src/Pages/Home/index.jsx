e60f814f25a7c4ff60603a84c6397d0357c5b47f
Dante Sotelo <136533308+dxnte1234@users.noreply.github.com>
2024-09-11 21:44:14 -0600

    spaces and colors

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -51,11 +51,11 @@ function Home() {
             </div>
             <a
               className="flex items-center space-x-2 text-base sm:text-lg pt-10 text-white"
-              href="https://api.whatsapp.com/send?phone=5610544410&text=Hola%2C%20me%20gustaría%20saber%20cúales%20accesorios%20personalizados%20recomiendan%20para%20mejorar%20el%20estilo%20y%20la%20funcionalidad%20de%20mi%20motocicleta."
+              href="https://api.whatsapp.com/send?phone=5610544410&text=Hola%2C%20me%20gustaría%20saber%20cuáles%20accesorios%20personalizados%20recomiendan%20para%20mejorar%20el%20estilo%20y%20la%20funcionalidad%20de%20mi%20motocicleta."
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="animate-bounce font-bold text-[#0eff06] hover:text-whote">
+              <button className="animate-bounce font-bold text-[#0eff06] hover:text-white">
                 Contáctanos para pedidos personalizados
               </button>
               <IoLogoWhatsapp
@@ -67,19 +67,19 @@ function Home() {
         </div>
         <div className="bg-black text-white py-5 flex flex-col items-center justify-center">
           <div className="text-center text-white max-w-4xl space-y-4 py-5 mb-8">
-            <h2 className="text-4xl sm:text-3xl md:text-6xl font-bold mb-4 text-[#0eff06] border-[#0eff06] p-2 py-10">
+            <div className="border-b-2 border-[#0eff06] p-2 text-lg sm:text-xl md:text-2xl py-10">
+            <h2 className="text-4xl sm:text-3xl md:text-6xl font-bold mb-1 text-[#0eff06] border-[#0eff06] p-2 py-10">
               Quiénes somos
             </h2>
-            <div className="border-b-2 border-[#0eff06] p-2 text-lg sm:text-xl md:text-2xl py-10">
-              <p className="text-sm sm:text-3x1 md:text-xl">
+              <p className="text-sm sm:text-3xl md:text-xl">
                 Somos ARS Racing, tu taller de personalización para
                 motocicletas. Nos dedicamos a crear piezas de alta calidad,
                 diseñadas para proteger y personalizar tu moto.
               </p>
-              <h2 className="text-4xl sm:text-3xl md:text-6xl font-bold mb-4 text-[#0eff06] border-[#0eff06] py-10">
+              <h2 className="text-4xl sm:text-3xl md:text-6xl font-bold mb-1 text-[#0eff06] border-[#0eff06] py-10">
                 Nuestra misión
               </h2>
-              <p className="text-sm sm:text-3x1 md:text-xl">
+              <p className="text-sm sm:text-3xl md:text-xl">
                 Nuestra pasión es proporcionar a los motociclistas de toda la
                 República Mexicana accesorios de alta calidad, diseñados para
                 mejorar tu experiencia de conducción, garantizando la seguridad
@@ -89,7 +89,7 @@ function Home() {
                 accesible para todos los apasionados por las motocicletas.
               </p>
             </div>
-            <SlBadge size="40px" className="text-[#0eff06] inline-block " />
+            <SlBadge size="60px" className="text-[#0eff06] inline-block " />
           </div>
         </div>
         <div className="bg-black flex flex-col items-center justify-center h-screen">
@@ -99,20 +99,20 @@ function Home() {
             <br />
             Contamos con:
           </div>
-          <div className="text-white text-justify w-10/12 mb-4 text-sm md:text-1xl lg:text-xl lg:w-1/2 md:w-1/2">
+          <div className="text-white text-justify w-10/12 mb-4 text-sm md:text-xl lg:text-xl lg:w-1/2 md:w-1/2">
             <br />{" "}
-            <strong className="text-green-300">
+            <strong className="text-[#0eff06]">
               - Parrillas deportivas y de carga{" "}
             </strong>
             pide la que mejor se ajuste a tus necesidades
             <br />{" "}
-            <strong className="text-green-300">
+            <strong className="text-[#0eff06]">
               - Sliders delanteros y traseros
             </strong>{" "}
-            que te ayudaran a proteger tu moto de caidas
-            <br /> <strong className="text-green-300">- Cubre faros</strong> que
-            le pueden dar a tu moto un estilo unico y de paso proteccion
-            antirobo <br /> <strong className="text-green-300">Y mas...</strong>
+            que te ayudarán a proteger tu moto de caídas
+            <br /> <strong className="text-[#0eff06]">- Cubre faros</strong> que
+            le pueden dar a tu moto un estilo único y de paso protección
+            antirrobo <br /> <strong className="text-[#0eff06]">Y más...</strong>
           </div>
           <Carousel />
         </div>
