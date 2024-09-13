2f642fab3873d6c8c509f6f7f71bc8091ab8fa0b
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-09-12 22:25:19 -0600

    change cards, delete from footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -12,52 +12,6 @@ export const Footer = () => {
 
   return (
     <footer>
-      <div className="relative flex flex-col lg:flex-row justify-around items-center text-black px-4 md:px-10 bg-black my-4 lg:mx-40 2xl:mx-96 *:">
-        {/* Tarjeta de pagos */}
-        <div className="border-2 border-[#0eff06] rounded-lg w-full max-w-xs md:max-w-sm lg:w-80 h-48 bg-white text-justify card-compact mb-4 lg:mb-0">
-          <h3 className="border-b-2 border-[#0eff06] text-center font-bold p-2">
-            Pagos a crédito o débito
-          </h3>
-          <p className="flex flex-row justify-items-center mt-2 mr-4">
-            <img
-              className="w-20 h-10 mt-6 mr-2"
-              src="https://avatarmotoapi.s3.us-east-2.amazonaws.com/Assets/cardcredit.png"
-            />
-            Con Mercado Pago, tienes pagos con tarjetas de débito y meses sin
-            intereses con tarjeta de crédito. ¡Y siempre es seguro!
-          </p>
-        </div>
-
-        {/* Tarjeta de compras protegidas */}
-        <div className="border-2 border-[#0eff06] rounded-lg w-full max-w-xs md:max-w-sm lg:w-80 h-48 bg-white text-justify card-compact mb-4 lg:mb-0">
-          <h3 className="border-b-2 border-[#0eff06] text-center font-bold p-2">
-            Compras protegidas
-          </h3>
-          <p className="flex flex-row justify-items-center mt-2 mr-4">
-            <img
-              className="w-20 h-16 mt-4 mr-2"
-              src="https://avatarmotoapi.s3.us-east-2.amazonaws.com/Assets/locked.webp"
-            />
-            Te acompañaremos hasta que recibas lo que compraste. Y si no es lo
-            que esperabas, te devolvemos el dinero.
-          </p>
-        </div>
-        {/* Tarjeta de envíos seguros */}
-        <div className="border-2 border-[#0eff06] rounded-lg w-full max-w-xs md:max-w-sm lg:w-80 h-48 bg-white text-justify card-compact">
-          <h3 className="border-b-2 border-[#0eff06] text-center font-bold p-2">
-            Envíos seguros
-          </h3>
-          <p className="flex flex-row justify-items-center mt-2 mr-4">
-            <img
-              className="w-20 h-20 mt-2 mr-2"
-              src="https://avatarmotoapi.s3.us-east-2.amazonaws.com/Assets/truck1.jpg"
-            />
-            Elige Mercado Envíos y sigue tu compra hasta que llegue a tus manos.
-            Tienes envíos gratis en productos seleccionados.
-          </p>
-        </div>
-      </div>
-
       <div className="w-full bg-black text-sm text-white flex flex-col md:flex-row justify-between items-start p-6">
         <div className="w-full flex flex-col md:flex-row justify-between md:gap-20 p-3 border-t-2 border-[#0eff06]">
           <div className="flex items-center border-b-2 md:border-b-0 md:border-r-2 border-[#0eff06] p-3 md:p-9 h-full">
